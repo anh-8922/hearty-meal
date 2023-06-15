fef65d36001713cc3dfebf5a3d7b75faaa73fa08
Anh Chau <anhchau8922@gmail.com>
2023-06-15 17:20:09 +0100

    last merge

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,8 +1,7 @@
-
 import AddShoppingList from '../Components/AddShoppingList';
 import useFetchCards from "../CustomHook/FetchData";
 import Spinner from "../Components/Spinner";
-import "../Components/Category/category.css";
+//import "../Components/Category/category.css";
 import Linkify from 'react-linkify';
 import MainLayout from "../Layouts/MainLayout";
 
@@ -36,46 +35,55 @@ export default function UserPage() {
 
   return(
     <MainLayout>
-      <div className="UserPage">
-        <div>
-        <div className="my-recipes">My Recipe</div>
-        <div className="category-group">
-                    {appetizerCard.map((item) => {
-                      const { id, itemImage, itemTitle, itemIngredients, itemInstructions } = item;
-                      return (
-                      
-                                <div key={id} className="item-card" style={{backgroundColor: '#D4C79E', width:'70%', borderRadius:'1rem'}}>
-                                      <h1>{itemTitle}</h1>
-                                      <div className="group-1">
-                                        <p>
-                                          <img src={itemImage} alt={itemTitle} style={{ width: '400px', height: '300px' }} />
-                                        </p>
-                                        <ul>
-                                          {itemIngredients.map((ingredient, index) => (
-                                            <li key={index} 
-                                                className="dot-list"><span>{ingredient}</span>
+      <div style={{
+        backgroundColor: 'whitesmoke',
+        width: '85%',
+        height: '118rem',
+        display: 'flex',
+        flexDirection: 'row',
+        marginRight: '2rem',
+        borderRadius: '0.7rem'
+    }}>
+      <div className="my-recipe">
+          
+          {appetizerCard.map((item) => {
+            const { id, itemImage, itemTitle, itemIngredients, itemInstructions } = item;
+            return (
+                      <div>
+                          <h1>My Recipes</h1>
+                          <div key={id} className="myrecipe-card">
+                                
+                                <h1>{itemTitle}</h1>
+                                <div className="myrecipe-body">
+                                  <p>
+                                    <img src={itemImage} alt={itemTitle} style={{ width: '25rem', height: '17rem' }} />
+                                  </p>
+                                  <ul>
+                                    {itemIngredients.map((ingredient, index) => (
+                                      <li key={index} 
+                                          className="dot-list"><span style={{fontSize: '15px'}}>{ingredient}</span>
 
-                                            </li>
-                                          ))}
-                                        </ul>
-                                      </div>
-                                      <div>
-                                        <pre style={{ whiteSpace: 'pre-wrap', fontFamily: 'sans-serif', fontSize: '1rem', padding: '1rem 2rem',   }}>
-                                          <CustomLinkify >{itemInstructions}</CustomLinkify>
-                                        </pre>
-                                      </div>
+                                      </li>
+                                    ))}
+                                  </ul>
                                 </div>
+                                <div>
+                                  <pre style={{ whiteSpace: 'pre-wrap', fontFamily: 'Raleway', fontSize: '15px', padding: '1rem 2rem',   }}>
+                                    <CustomLinkify >{itemInstructions}</CustomLinkify>
+                                  </pre>
+                                </div>
+                          </div>
+                      </div>
+                     
 
-                                          
-                      );
-                    })}
-        </div>
-        </div>
-        <div>
-          <div>Favourites</div>
-          <div><AddShoppingList/></div>
-        </div>
+                                
+            );
+          })}
+          
+        </div>  
+        <div  style={{margin: '1rem'}}><AddShoppingList/></div>
       </div>
+
     </MainLayout>
   )
   
